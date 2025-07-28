856d99e1bdd27e006859da5b45038a54c2e8f4ee
amcar-dtu <amcar@dtu.dk>
2025-07-28 18:03:40 +0200

    Refactor Mendeley catalog script to remove DOI argument and improve config loading

--- mendeley-catalog.py
+++ mendeley-catalog.py
@@ -5,7 +5,7 @@ import os
 # Get the DOI to look up
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("doi", help="Searches the Mendeley catalogue for this DOI")
+# parser.add_argument("doi", help="Searches the Mendeley catalogue for this DOI")
 args = parser.parse_args()
 
 config_file = 'config.yml'
@@ -14,15 +14,9 @@ config = {}
 
 if os.path.isfile(config_file): 
     with open('config.yml') as f:
-        config = yaml.load(f)
-else:
-    config['clientId'] = os.environ.get('MENDELEY_CLIENT_ID')
-    config['clientSecret'] = os.environ.get('MENDELEY_CLIENT_SECRET')
+        config = yaml.safe_load(f)
 
 mendeley = Mendeley(config['clientId'], config['clientSecret'])
 session = mendeley.start_client_credentials_flow().authenticate()
 
-doi = args.doi
-
-doc = session.catalog.by_identifier(doi=doi, view='stats')
-print '"%s" has %s readers.' % (doc.title, doc.reader_count)
+print(f"Groups: {session.groups.list()}")
